d9db65242033955c296a22d598b6cdd228a1206b
ik3320 <61996797+ik3320@users.noreply.github.com>
2026-06-08 01:26:49 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ import sys
 from playwright.async_api import async_playwright
 
 # 깃허브 액션 연동을 고려하여 주소를 가져옵니다. (로컬 실행 시 주소를 직접 적으셔도 됩니다)
-GAS_WEBAPP_URL = os.environ.get("GAS_URL", "https://script.google.com/macros/s/AKfycbyh42kxRzPkHYedYaHl_M98ECqti9pYuYfdeYNvP4VPr_wsb9-oieHPUr8uM9YLACrJLA/exec")
+GAS_WEBAPP_URL = os.environ.get("GAS_URL", "https://script.google.com/macros/s/AKfycbxagBJ1FrUDpPsHm88Nfn2mbqw9XW13jqnkGGW-iz9p7gBcnHLP2hoez1zOmHCy355hHA/exec")
 
 if not GAS_WEBAPP_URL:
     print("오류: 구글 웹 앱 URL(GAS_URL)이 세팅되지 않았습니다.")
